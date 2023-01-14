33f62f778da95856c6cbf69f606ad93613f41443
roycehoe <royce@patsnap.com>
2023-01-14 15:13:59 +0800

    Implement unit tests to bring code coverage to 100%

--- test/test_gilded_rose.py
+++ test/test_gilded_rose.py
@@ -1,7 +1,91 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+import pytest
+
 from gilded_rose import GildedRose, Item
+
+
+@pytest.fixture
+def mock_items() -> list[Item]:
+    return [
+        Item(name="+5 Dexterity Vest", sell_in=10, quality=20),
+        Item(name="Aged Brie", sell_in=2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=3, quality=6),  # <-- :O
+    ]
+
+
+@pytest.fixture
+def mock_negative_items() -> list[Item]:
+    return [
+        Item(name="+5 Dexterity Vest", sell_in=-10, quality=20),
+        Item(name="Aged Brie", sell_in=-2, quality=0),
+        Item(name="Elixir of the Mongoose", sell_in=-5, quality=7),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
+        Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=-15, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=-10, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=-5, quality=20),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=-10, quality=49),
+        Item(name="Backstage passes to a TAFKAL80ETC concert", sell_in=-5, quality=49),
+        Item(name="Conjured Mana Cake", sell_in=-3, quality=6),  # <-- :O
+    ]
+
+
+def test_item_quality_reduces_by_one(mock_items):
+    gilded_rose = GildedRose(mock_items)
+
+    assert gilded_rose.items[0].quality == 20
+    gilded_rose.update_quality()
+    assert gilded_rose.items[0].quality == 19
+
+
+def test_backstage_item_increaes_in_quality_as_sell_in_date_approaches_medium_sell_in_low_quantity(
+    mock_items: list[Item],
+):
+    gilded_rose = GildedRose(mock_items)
+    backstage_item = gilded_rose.items[6]
+
+    assert backstage_item.name == "Backstage passes to a TAFKAL80ETC concert"
+    assert backstage_item.quality == 20
+    assert backstage_item.sell_in == 10
+    gilded_rose.update_quality()
+    assert backstage_item.quality == 22
+
+
+def test_backstage_item_increaes_in_quality_as_sell_in_date_approaches_low_sell_in_low_quantity(
+    mock_items: list[Item],
+):
+    gilded_rose = GildedRose(mock_items)
+    backstage_item = gilded_rose.items[7]
+
+    assert backstage_item.name == "Backstage passes to a TAFKAL80ETC concert"
+    assert backstage_item.quality == 20
+    assert backstage_item.sell_in == 5
+    gilded_rose.update_quality()
+    assert backstage_item.quality == 23
+
+
+def test_negative_sell_in_reduces_quality_twice_as_fast(
+    mock_negative_items: list[Item],
+):
+    gilded_rose = GildedRose(mock_negative_items)
+    negative_sell_in_item = gilded_rose.items[0]
+
+    assert negative_sell_in_item.sell_in < 0
+    assert negative_sell_in_item.quality == 20
+
+    gilded_rose.update_quality()
+
+    assert negative_sell_in_item.quality == 18
 
 
 class GildedRoseTest(unittest.TestCase):
@@ -9,11 +93,14 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
+        assert "foo" == items[0].name
 
     def test_repr_with_correct_arg_ordering(self):
-        item = Item("foo", 0, 0)
-        assert repr(item) == "foo, 0, 0"
+        assert repr(Item("foo", 0, 0)) == "foo, 0, 0"
+
+    # def test_repr_with_wrong_arg_ordering(self):
+    #     with pytest.raises(Exception):
+    #         item = Item(0, 0, "foo")
 
 
 if __name__ == "__main__":
